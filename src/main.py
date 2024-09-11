5b787e279e55e4b4016ab2e4a671c83db2c543d5
iamsuchafitta <inna.200156@gmail.com>
2024-09-12 01:39:29 +0300

    added a bug to the source code
    Пункт 33. Добавьте ошибку в тексты исходных кодов, зафиксируйте изменения в Git и дождитесь
    отрицательного результата автоматизированного тестирования. Ознакомьтесь с выводом скрипта
    тестирования и иконкой статуса. Удалите ошибку и вновь зафиксируйте изменения. Убедитесь в
    успешном прохождении тестирования проекта.

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def get_path_from_arguments(args) -> str:
 def main():
     path = get_path_from_arguments(sys.argv[1:])
     reader = TextDataReader()
-    students = reader.read(path)
+    #students = reader.read(path)
     print("Students: ", students)
 
     rating = CalcRating(students).calc()
